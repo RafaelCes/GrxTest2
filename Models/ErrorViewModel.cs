c16f10db165e931f6d35c9205762679cedbcce9d
rafaf <rafaf@DESKTOP-46JDP3U>
2021-12-21 23:20:46 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrxTest2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
